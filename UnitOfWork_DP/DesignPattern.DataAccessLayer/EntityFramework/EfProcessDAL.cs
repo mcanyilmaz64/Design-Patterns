d6a13cf8f7b1bb93171e4bf0cd99cb729e191463
Mahmut Can Yılmaz <mcyilmaz64@hotmail.com>
2026-03-17 06:20:04 +0300

    Generic Repository and EfDAL classes created

---
+++ UnitOfWork_DP/DesignPattern.DataAccessLayer/EntityFramework/EfProcessDAL.cs
@@ -0,0 +1,19 @@
+﻿using DesignPattern.DataAccessLayer.Abstract;
+using DesignPattern.DataAccessLayer.Concrete;
+using DesignPattern.DataAccessLayer.Repositories;
+using DesignPattern.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.EntityFramework
+{
+    public class EfProcessDAL : GenericRepository<Process>, IProcessDAL
+    {
+        public EfProcessDAL(Context context) : base(context)
+        {
+        }
+    }
+}
