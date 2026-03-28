a804185b5ff5a9628c04e197c2a5d012c0e9049e
Mahmut Can Yılmaz <mcyilmaz64@hotmail.com>
2026-03-29 01:06:11 +0300

    Category names appearing as dropdowns in product forms

--- Repository_DP/DesignPattern.Repository/Controllers/ProductController.cs
+++ Repository_DP/DesignPattern.Repository/Controllers/ProductController.cs
@@ -1,16 +1,19 @@
 using DesignPattern.Repository.Business.Abstract;
 using DesignPattern.Repository.Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DesignPattern.Repository.Controllers
 {
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         public IActionResult Index()
@@ -21,6 +24,13 @@ namespace DesignPattern.Repository.Controllers
 
         public IActionResult Create()
         {
+            List<SelectListItem> values = (from x in _categoryService.TGetAll()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
@@ -33,6 +43,13 @@ namespace DesignPattern.Repository.Controllers
 
         public IActionResult Edit(int id)
         {
+            List<SelectListItem> categories = (from x in _categoryService.TGetAll()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value = x.CategoryId.ToString()
+                                               }).ToList();
+            ViewBag.v = categories;
             var values = _productService.TGetById(id);
             return View(values);
         }
